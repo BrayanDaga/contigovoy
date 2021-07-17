180a54b0dfa5ced78c1ff429128fd7d4cfc78516
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-17 17:34:22 -0500

    Se-agregaron-paginas

---
+++ views/contents/web-modalidad.php
@@ -0,0 +1,9 @@
+    <!-- Page Content -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-header">Modalidad de Trabajo</h1>
+                Pagina  Modalidad de Trabajo
+            </div>
+        </div>
+    </div>
--- views/contents/web-parati.php
+++ views/contents/web-parati.php
@@ -2,8 +2,8 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="page-header">Contactanos</h1>
-                Pagina  Contactanos
+                <h1 class="page-header">Para ti</h1>
+                Pagina  Para ti
             </div>
         </div>
     </div>
