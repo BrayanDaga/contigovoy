db8e8fef4481fb7eae1310c4f6fad6ae15fb237d
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-18 17:34:00 -0500

    SliderCard

--- views/contents/web-conocenos.php
+++ views/contents/web-conocenos.php
@@ -1,127 +1,163 @@
 <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <!-- Card Slider Bootstrap4 -->
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person One</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Card Slider Bootstrap4 -->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators ">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Two</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
+                                    <div class="card-header">
+                                        <h3>Persona 1</h3>
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Three</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Four</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 2</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
                                     </div>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Five</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Six</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
-                                    </div>
-                                </div>
+
                             </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Seven</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 3</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
                                     </div>
                                 </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Eigth</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="single-box">
-                                        <div class="img-area">
-                                            <img src="https://i.pravatar.cc/300" alt="https://i.pravatar.cc/300">
-                                        </div>
-                                        <div class="img-text">
-                                            <h2>Person Nine</h2>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                                        </div>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h3>Persona 4</h3>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 5</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 6</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                        <br>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h3>Persona 7</h3>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 8</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <img src="https://i.pravatar.cc/300" class="img-fluid">
+                                    </div>
+                                    <div class="card-header">
+                                        <h2>Persona 9</h2>
+                                    </div>
+                                    <div class="card-footer">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, error dolorum. Iure, quia nostrum temporibus neque quisquam possimus voluptatem harum qui repudiandae quas architecto sequi minima tempore aliquam aliquid debitis!</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
                 </div>
             </div>
         </div>
     </div>
+</div>
