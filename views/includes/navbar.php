023e2525f59fb634282528dcd5476d604ea74a89
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-18 21:54:42 -0500

    localhost/contigovoy--defecto

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -12,10 +12,10 @@
         <!--container-flex-bootstrap-->
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav active">
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/'  ? 'active' : '' ?>"><a href="/" class="nav-link">Home <i class="fas fa-home"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/'  ? 'active' : '' ?>"><a href="/" class="nav-link">Home&nbsp;<i class="fas fa-home"></i></a>
                 </li>
                 <li class="nav navbar-nav dropdown <?= $_SERVER['REQUEST_URI']==='/servicios'  ? 'active' : '' ?>">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/servicios" role="button" aria-haspopup="true" aria-expanded="false">Servicios <i class="fas fa-list"></i></a>
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/servicios" role="button" aria-haspopup="true" aria-expanded="false">Servicios&nbsp;<i class="fas fa-list"></i></a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="/servicios#lenguaje">Terapia de lenguaje</a>
                         <a class="dropdown-item" href="/servicios#emocional">Terapia Emocional</a>
@@ -24,18 +24,17 @@
                         <a class="dropdown-item" href="/servicios#psicoterapia">Psicoterapia</a>
                     </div>
                 </li>
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/conocenos'  ? 'active' : '' ?>"><a href="/conocenos" class="nav-link">Conocenos <i class="fas fa-user"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/conocenos'  ? 'active' : '' ?>"><a href="/conocenos" class="nav-link">Conocenos&nbsp;<i class="fas fa-user"></i></a>
                 </li>
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/modalidad'  ? 'active' : '' ?>"><a href="/modalidad" class="nav-link">Modalidad de Trabajo <i class="fas fa-chart-network"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/modalidad'  ? 'active' : '' ?>"><a href="/modalidad" class="nav-link">Modalidad de Trabajo&nbsp;<i class="fas fa-chart-network"></i></a>
                 </li>
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contactanos'  ? 'active' : '' ?>"><a href="/contactanos" class="nav-link">Contactanos <i class="far fa-comment-dots"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contactanos'  ? 'active' : '' ?>"><a href="/contactanos" class="nav-link">Contactanos&nbsp;<i class="far fa-comment-dots"></i></a>
                 </li>
                 <li class="nav navbar-nav dropdown <?= $_SERVER['REQUEST_URI']==='/parati'  ? 'active' : '' ?>">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/parati" role="button" aria-haspopup="true" aria-expanded="false">Para ti <i class="fas fa-hand-point-down"></i></a>
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/parati" role="button" aria-haspopup="true" aria-expanded="false">Para ti&nbsp;<i class="fas fa-hand-point-down"></i></a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="/parati#material">Material Didáctico</a>
                         <a class="dropdown-item" href="/parati#blog">Blog</a>
-                        <a class="dropdown-item" href="/parati#otros">Otros</a>
                     </div>
                 </li>
             </ul>
