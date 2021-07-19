7423ae657a09ab44d000a6fe271abe65295cddc8
mr01robot <42098976+mr01robot@users.noreply.github.com>
2021-07-19 17:41:17 -0500

    penultimo push

--- views/includes/slider.php
+++ views/includes/slider.php
@@ -7,17 +7,27 @@
                       <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
                     <div class="carousel-inner">
+                
                       <div class="carousel-item active">
+                     
                         <img src="https://picsum.photos/800/250" class="d-block w-100" alt="https://picsum.photos/800/250">
-                        <div class="carousel-caption d-none d-md-block">
-                          <div class="form">
-                            <input type="text">
-                            <input type="text">
-                            <input type="text">
-                            <input type="text">
-                          </div>
-                          <h5>First slide label</h5>
-                          <p>Some representative placeholder content for the first slide.</p>
+                    
+                        <div class="carousel-caption d-none d-md-block form1">
+                       
+            
+             
+                        <form class="formulario">
+                          <h1>APRENDE CON</h1>
+                          <h3>nosotros de la mejor forma</h3>
+                          <p>Dejanos tus datos para ponernos en contacto contigo y asi recibir más informacion y conocernos más</p>
+                            <input type="text" placeholder="Correo electronico"/>  <br/><br/>
+                            <input type="text" placeholder="Núnmero de contacto"/>   <br/><br/>
+                            
+                           <input class="sumit" type="submit" value="Enviar"> 
+                        </form>
+             
+                       
+                         
                         </div>
                       </div>
                       <div class="carousel-item">
