1f27d065de3e4940970a63379ae8966e3e928004
mr01robot <42098976+mr01robot@users.noreply.github.com>
2021-07-22 13:47:23 -0500

    i

--- views/contents/web-modalidad.php
+++ views/contents/web-modalidad.php
@@ -7,7 +7,8 @@
                 <h3 class="page-header">Si necesitas algún servicio de CONTIGO VOY</h3>
             
             </div>
-            <div class="col-md-12">
+
+        
                 <div class="col-md-7 text">
                     <p>Te invitamos a conocer nuestros servicios de CONTIGO VOY: terapia de lenguaje, aprendizaje, psicología en general ,venta de materiales, etc.
                         En tu primera sesión, realizaremos una entrevista para saber ¿cual es tu motivo de consulta? con el objetivo de trabajar un plan de tratamiento de acuerdo a los resultados obtenidos durante las entrevistas y las evaluaciones
@@ -17,5 +18,7 @@
                     <img src="<?= IMAGES ?>/psico.webp" alt="">
                 </div>
             </div>
+ 
         </div>
  </div>
+
--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -6,8 +6,8 @@
         <div><a href="#"><i class="fab fa-tiktok"></i></a></div>
         <div><a href="#"><i class="fab fa-youtube"></i></a></div>
         <div><a href="#"><i class="fas fa-envelope"></i></a></div>
-        <div><a href="#"><i class="fab fa-instagram-square"></i></a></div>
-        <div><a href="#"><i class="fab fa-facebook-square"></i></a></div>
+        <div><a href="https://www.instagram.com/contigovoyperu/"><i class="fab fa-instagram-square"></i></a></div>
+        <div><a href="https://www.facebook.com/Contigo-voy-108528551483244"><i class="fab fa-facebook-square"></i></a></div>
     </div>
 <nav class="navbar navbar-light bg-light navbar-expand-md">
 
@@ -24,17 +24,10 @@
         <!--container-flex-bootstrap-->
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav active">
-<<<<<<< HEAD
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contigovoy'  ? 'active' : '' ?> li"><a href="/contigovoy/" class="nav-link">Home&nbsp;<i class="fas fa-home"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/'  ? 'active' : '' ?> li"><a href="/" class="nav-link">INICIO&nbsp;<i class="fas fa-home"></i></a>
                 </li>
                 <li class="nav navbar-nav dropdown <?= $_SERVER['REQUEST_URI']==='/servicios'  ? 'active' : '' ?> li">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/contigovoy/servicios" role="button" aria-haspopup="true" aria-expanded="false">Servicios&nbsp;<i class="fas fa-list"></i></a>
-=======
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/'  ? 'active' : '' ?>"><a href="/" class="nav-link">Home&nbsp;<i class="fas fa-home"></i></a>
-                </li>
-                <li class="nav navbar-nav dropdown <?= $_SERVER['REQUEST_URI']==='/servicios'  ? 'active' : '' ?>">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/servicios" role="button" aria-haspopup="true" aria-expanded="false">Servicios&nbsp;<i class="fas fa-list"></i></a>
->>>>>>> f6019caf8f0be7b68eec3199c09477f95f2af4c4
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/servicios" role="button" aria-haspopup="true" aria-expanded="false">SERVICIOS&nbsp;<i class="fas fa-list"></i></a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="/servicios#lenguaje">Terapia de lenguaje</a>
                         <a class="dropdown-item" href="/servicios#emocional">Terapia Emocional</a>
@@ -43,30 +36,17 @@
                         <a class="dropdown-item" href="/servicios#otros">Otros</a>
                     </div>
                 </li>
-<<<<<<< HEAD
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/conocenos'  ? 'active' : '' ?> li"><a href="/contigovoy/conocenos" class="nav-link">Conocenos&nbsp;<i class="fas fa-user"></i></a>
-=======
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/conocenos'  ? 'active' : '' ?>"><a href="/conocenos" class="nav-link">Conocenos&nbsp;<i class="fas fa-user"></i></a>
->>>>>>> f6019caf8f0be7b68eec3199c09477f95f2af4c4
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/conocenos'  ? 'active' : '' ?> li"><a href="/conocenos" class="nav-link">CONOCENOS&nbsp;<i class="fas fa-user"></i></a>
                 </li>
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/modalidad'  ? 'active' : '' ?> li"><a href="/contigovoy/modalidad" class="nav-link">Modalidad de Trabajo&nbsp;<i class="fas fa-chart-network"></i></a>
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/modalidad'  ? 'active' : '' ?> li"><a href="/contigovoy/modalidad" class="nav-link">MODALIDAD DE TRABAJO&nbsp;<i class="fas fa-chart-network"></i></a>
                 </li>
-<<<<<<< HEAD
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contactanos'  ? 'active' : '' ?> li"><a href="/contigovoy/contactanos" class="nav-link">Contactanos&nbsp;<i class="far fa-comment-dots"></i></a>
-=======
-                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contactanos'  ? 'active' : '' ?>"><a href="/contactanos" class="nav-link">Contactanos&nbsp;<i class="far fa-comment-dots"></i></a>
->>>>>>> f6019caf8f0be7b68eec3199c09477f95f2af4c4
+                <li class="nav-item <?= $_SERVER['REQUEST_URI']==='/contactanos'  ? 'active' : '' ?> li"><a href="/contactanos" class="nav-link">CONTACTANOS&nbsp;<i class="far fa-comment-dots"></i></a>
                 </li>
                 <li class="nav navbar-nav dropdown <?= $_SERVER['REQUEST_URI']==='/parati'  ? 'active' : '' ?> li">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/parati" role="button" aria-haspopup="true" aria-expanded="false">Para ti&nbsp;<i class="fas fa-hand-point-down"></i></a>
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/parati" role="button" aria-haspopup="true" aria-expanded="false">PARAT TI&nbsp;<i class="fas fa-hand-point-down"></i></a>
                     <div class="dropdown-menu">
-<<<<<<< HEAD
-                        <a class="dropdown-item" href="/contigovoy/parati#material">Material Didáctico</a>
-                        <a class="dropdown-item" href="/contigovoy/parati#blog">Blog</a>
-=======
                         <a class="dropdown-item" href="/parati#material">Material Didáctico</a>
                         <a class="dropdown-item" href="/parati#blog">Blog</a>
->>>>>>> f6019caf8f0be7b68eec3199c09477f95f2af4c4
                     </div>
                 </li>
             </ul>
