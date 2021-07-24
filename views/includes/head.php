3ff91acfc7fa928d8d01b7de6268ec088d24b794
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-23 20:12:00 -0500

    carga-imagenes

--- views/includes/head.php
+++ views/includes/head.php
@@ -21,3 +21,18 @@
 <link rel="stylesheet" href="<?= CSS; ?>modalidad.css">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<script>  
+function ReLoadImages(){
+    $('img[data-lazysrc]').each( function(){
+        //* set the img src from data-src
+        $( this ).attr( 'src', $( this ).attr( 'data-lazysrc' ) );
+        }
+    );
+}
+
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === "interactive") {  //or at "complete" if you want it to execute in the most last state of window.
+        ReLoadImages();
+    }
+});
+</script>
