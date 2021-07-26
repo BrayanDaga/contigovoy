93b0c282f3fb41f45cab4e7fd34dd9db57978908
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-25 23:58:12 -0500

    cambios-inicio

--- views/contents/web-index.php
+++ views/contents/web-index.php
@@ -1,18 +1,67 @@
-<!-- Page Content -->
-
-
-
-
-
-<div class="container-fluid">
-    <!--<div class="row">
+   <!--<div class="row">
         <div class="col-12">
             <h1 class="page-header">Home </h1>
             Welcome to the Home Page
         </div>
 
     </div>-->
-    <?php include_once INCLUDES .'slider.php'; ?>
+   <?php include_once INCLUDES . 'slider.php'; ?>
 
-</div>
-</body>
+
+
+
+   <section class="bg-primary p-5 ">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="text-center  text-white  mx-2">
+                       <h2>Nuestros Servicios</h2>
+                       <p>
+                           Somos especialistas en el comportamiento humano y desarrollo de competencias, contamos con un equipo de psicólogos especialistas que te ayudaran a tener una mejor calidad de vida. Nuestro Centro psicológico y nuestras asesorías a distintas empresas a nivel nacional aseguran la calidad de nuestro servicio
+                       </p>
+                   </div>
+               </div>
+           </div>
+           <div class="row text-secondary mt-2">
+
+
+               <div class="col-md-4">
+                   <div class="card text-center shadow my-2">
+                       <div class="card-body">
+                           <h4 class="card-title">Psicología</h4>
+                           <p class="card-text">
+                               Somos especialistas en el comportamiento humano y desarrollo de competencias, contamos con un equipo de psicólogos especialistas que te ayudaran a tener una mejor calidad de vida. Nuestro Centro psicológico y nuestras asesorías a distintas empresas a nivel nacional aseguran la calidad de nuestro servicio.
+                           </p>
+                       </div>
+                   </div>
+               </div>
+              
+               <div class="col-md-4">
+                   <div class="card text-center shadow my-2">
+                       <div class="card-body">
+                           <h4 class="card-title">Psicología</h4>
+                           <p class="card-text">
+                               Somos especialistas en el comportamiento humano y desarrollo de competencias, contamos con un equipo de psicólogos especialistas que te ayudaran a tener una mejor calidad de vida. Nuestro Centro psicológico y nuestras asesorías a distintas empresas a nivel nacional aseguran la calidad de nuestro servicio.
+                           </p>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-4">
+                   <div class="card text-center shadow my-2">
+                       <div class="card-body">
+                           <h4 class="card-title">Psicología</h4>
+                           <p class="card-text">
+                               Somos especialistas en el comportamiento humano y desarrollo de competencias, contamos con un equipo de psicólogos especialistas que te ayudaran a tener una mejor calidad de vida. Nuestro Centro psicológico y nuestras asesorías a distintas empresas a nivel nacional aseguran la calidad de nuestro servicio.
+                           </p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+
+       </div>
+   </section>
+
+
+
+   </body>
--- views/includes/slider.php
+++ views/includes/slider.php
@@ -1,65 +1,62 @@
-    <!--bootstrap4-->
-    <div class="col-md-12">
-                <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                      <li data-target="#myCarousel" data-slide-to="1"></li>
-                      <li data-target="#myCarousel" data-slide-to="2"></li>
-                      <li data-target="#myCarousel" data-slide-to="3"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                
-                      <div class="carousel-item active">
-                     
-                        <img src="<?= IMAGES ?>/img1.webp" class="d-block w-100" alt="contigo">
-              
-                        <div class="carousel-caption d-none d-md-block form1">
-                       
-            
-             
-                        <form class="formulario">
-                          <h1>APRENDE CON</h1>
-                          <h3>nosotros de la mejor forma</h3>
-                          <p>Dejanos tus datos para ponernos en contacto contigo y asi recibir más informacion y conocernos más</p>
-                            <input type="text" placeholder="Correo electronico"/>  <br/><br/>
-                            <input type="text" placeholder="Núnmero de contacto"/>   <br/><br/>
-                            
-                           <input class="sumit" type="submit" value="Enviar"> 
-                        </form>
-             
-                       
-                         
-                        </div>
-                      </div>
-                      <div class="carousel-item">
-                      <img src="<?= IMAGES ?>/img2.webp" class="d-block w-100" alt="terapia">
-                          <!-- <div class="carousel-caption d-none d-md-block">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+
+      <img src="<?= IMAGES ?>/img1.webp" class="d-block w-100" alt="contigo">
+
+      <div class="carousel-caption d-none d-md-block form1">
+
+
+
+        <form class="formulario">
+          <h1>APRENDE CON</h1>
+          <h3>nosotros de la mejor forma</h3>
+          <p>Dejanos tus datos para ponernos en contacto contigo y asi recibir más informacion y conocernos más</p>
+          <input type="text" placeholder="Correo electronico" /> <br /><br />
+          <input type="text" placeholder="Núnmero de contacto" /> <br /><br />
+
+          <input class="sumit" type="submit" value="Enviar">
+        </form>
+
+
+
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?= IMAGES ?>/img2.webp" class="d-block w-100" alt="terapia">
+      <!-- <div class="carousel-caption d-none d-md-block">
                           <h5>Second slide label</h5>
                           <p>Some representative placeholder content for the second slide.</p>
                         </div>-->
-                      </div>
-                      <div class="carousel-item">
-                      <img src="<?= IMAGES ?>/img3.webp" class="d-block w-100" alt="didactico">
-                          <!-- <div class="carousel-caption d-none d-md-block">
+    </div>
+    <div class="carousel-item">
+      <img src="<?= IMAGES ?>/img3.webp" class="d-block w-100" alt="didactico">
+      <!-- <div class="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                           <p>Some representative placeholder content for the third slide.</p>
                         </div>-->
-                      </div>
-                      <div class="carousel-item">
-                      <img src="<?= IMAGES ?>/img4.webp" class="d-block w-100" alt="material">
-                          <!-- <div class="carousel-caption d-none d-md-block">
+    </div>
+    <div class="carousel-item">
+      <img src="<?= IMAGES ?>/img4.webp" class="d-block w-100" alt="material">
+      <!-- <div class="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                           <p>Some representative placeholder content for the third slide.</p>
                         </div>-->
-                      </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </div>
-            </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
