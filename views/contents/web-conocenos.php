481a3d382ae41b3646128fe1986899da1160c00d
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-08-04 12:40:23 -0500

    fix-conocenos

--- views/contents/web-conocenos.php
+++ views/contents/web-conocenos.php
@@ -1,48 +1,45 @@
-<div class="container">
-    <div class="d-flex justify-content-around">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <img src="https://i.pravatar.cc/300" class="img-fluid islider">
-                </div>
-                <div class="card-header">
-                    <h3>Wendy Aylas Martínez</h3>
-                </div>
-                <div class="card-footer">
-                    <p>-Licenciada en Psicologpia <br>-Psicoterapeuta conductual congnitivo <br> -Especialista en terapia de lenguaje <br>-Especialista en intervención de problemas de aprendizaje. </p>
-                    <br>
-                </div>
+<!-- Page Content -->
+<main>
+    <div class="container-fluid">
+    <h1 class="page-header text-center text-primary font-weight-bold">CONOCENOS</h1>
+    <hr>
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-8">
+           <div class="row justify-content-around">
+           <div class="card">
+            <div class="card-body">
+                <img src="https://i.pravatar.cc/300" class="img-fluid islider">
+            </div>
+            <div class="card-header">
+                <h3>Wendy Aylas Martínez</h3>
+            </div>
+            <div class="card-footer">
+                <p>-Licenciada en Psicologpia <br>-Psicoterapeuta conductual congnitivo <br> -Especialista en terapia de lenguaje <br>-Especialista en intervención de problemas de aprendizaje. </p>
+                <br>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <img src="https://i.pravatar.cc/300" class="img-fluid islider">
-                </div>
-                <div class="card-header">
-                    <h2>Ana Vizcarra Sajami</h2>
-                </div>
-                <div class="card-footer">
-                    <p>-Licenciada en Psicología <br>-Psicoterapeuta conductual cognitivo <br>-Especialista en terapia de lenguaje <br> Especialista en intervención de problemas de aprendizaje</p>
-                    <br>
-                </div>
+        <div class="card">
+            <div class="card-body">
+                <img src="https://i.pravatar.cc/300" class="img-fluid islider">
             </div>
-
+            <div class="card-header">
+                <h2>Ana Vizcarra Sajami</h2>
+            </div>
+            <div class="card-footer">
+                <p>-Licenciada en Psicología <br>-Psicoterapeuta conductual cognitivo <br>-Especialista en terapia de lenguaje <br> Especialista en intervención de problemas de aprendizaje</p>
+                <br>
+            </div>
         </div>
-
+           </div>
+        </div>
+    </div>
     </div>
 
+    <section class="mt-3">
+                <img src="<?= IMAGES ?>/mision.png" class="img-fluid" width="100%">
+    </section>
+    <section class="mt-3">
+                <img src="<?= IMAGES ?>/vision.png" class="img-fluid" width="100%">
+    </section>
 
-
-</div>
-<div class="container-fluid cont m-auto text-center">
-    <div class="f">
-        <img src="<?= IMAGES ?>/mision.png" alt="">
-
-    </div>
-
-    <div class="f">
-        <img src="<?= IMAGES ?>/vision.png" alt="">
-
-    </div>
-</div>
+</main>
