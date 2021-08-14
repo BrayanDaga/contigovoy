effb97847709e2f7ba61d85eeb8b347986d44005
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-08-14 15:56:59 -0500

    Images

--- views/template.php
+++ views/template.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contigo Voy | {{ @title }} </title>
-    <link rel="shortcut icon" type="image/jpg" href="pu/logo.ico" />
+    <link rel="shortcut icon" type="image/jpg" href="public/img/logo.ico" />
     <link rel="stylesheet" href="public/css/app.css">
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
