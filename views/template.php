6536e48f09edd706a843cacab460caaf53bfcac5
Luis923 <luis_gaspar923@hotmail.com>
2021-08-18 22:53:05 -0500

    Se agrego gulp

--- views/template.php
+++ views/template.php
@@ -7,19 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contigo Voy | {{ @title }} </title>
     <link rel="shortcut icon" type="image/jpg" href="public/img/logo.ico" />
+    <link rel="stylesheet" href="build/css/app.css">
     <link rel="stylesheet" href="public/css/app.min.css">
-
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <include href="views/includes/navbar.php" />
     <script src="public/js/jquery-3.5.1.min.js"></script>
     <script src="public/js/bundle.js"></script>
+    <script src="build/js/bundle.min.js"></script>
 </head>
-
 <body>
     <include href="{{ 'views/'.@content }}" />
     <check if="{{ @content=='reservatucita.php' }}">
         <script type="text/javascript" src="public/js/reservacita.js"></script>
     </check>
 </body>
-
 </html>
