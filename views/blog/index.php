2863543d8366477b5b2c18430d9d54bd17a22a42
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-09-05 16:58:11 -0500

    Paginacion

--- views/blog/index.php
+++ views/blog/index.php
@@ -12,82 +12,61 @@
         <hr>
         <div data-cy="blogNewsCards" class="m-auto col-lg-10 mt-lg-n10">
 
-        <!-- Section de prueba eliminar cuando ya tengan con base de datos -->
-            <section class="card mb-5 shadow ">
-                <div class="card-body p-5">
-                    <div class="d-flex align-items-center">
-                        <div class="flex-shrink-0 d-none d-lg-block "><img class="img-fluid " src="https://picsum.photos/350/300" alt="undefined"></div>
-                        <div class="ml-3">
-                            <div class="mb-3">
-                                <h2 class="mb-0"><a href="#" class="text-danger">TÍTULO DEL ARTÍCULO DEL BLOG</a></h2>
-                                <!-- <div class="small text-muted mb-2">July 18, 2021</div> -->
-                                <div class="mb-3">
-                                    <!-- <a class="badge text-primary text-capitalize">news</a>
-                                    <a class="badge text-primary text-capitalize">forms</a> -->
-                                </div>
-                            </div>
-                            <div>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti earum corrupti sapiente amet tempora est quae, deserunt quasi eligendi, impedit repudiandae temporibus illo molestias possimus commodi iusto! Nihil, totam hic!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- end section -->
-                   
             <!-- Section de prueba eliminar cuando ya tengan con base de datos -->
-            <section class="card mb-5 shadow ">
-                <div class="card-body p-5">
-                    <div class="d-flex align-items-center">
-                        <div class="flex-shrink-0 d-none d-lg-block "><img class="img-fluid " src="https://picsum.photos/350/300" alt="undefined"></div>
-                        <div class="ml-3">
-                            <div class="mb-3">
-                                <h2 class="mb-0"><a href="#" class="text-danger">Titulo del Blog</a></h2>
-                                <!-- <div class="small text-muted mb-2">July 18, 2021</div> -->
-                                <div class="mb-3">
-                                    <!-- <a class="badge text-primary text-capitalize">news</a>
-                                    <a class="badge text-primary text-capitalize">forms</a> -->
-                                </div>
-                            </div>
-                            <div>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti earum corrupti sapiente amet tempora est quae, deserunt quasi eligendi, impedit repudiandae temporibus illo molestias possimus commodi iusto! Nihil, totam hic!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+          
             <!-- end section -->
 
-            <repeat group="{{ @blogs }}" value="{{ @blog }}">  <!-- Hace un bucle igual a foreach  --> 
+            <!-- Section de prueba eliminar cuando ya tengan con base de datos -->
+          
+            <!-- end section -->
+            <repeat group="{{ @blogs.subset  }}" value="{{ @blog }}">
+                <!-- Hace un bucle igual a foreach  -->
                 <section class="card mb-5 shadow ">
                     <div class="card-body p-5">
                         <div class="row">
                             <div class="col-4">
-                            <img class="img-fluid " src="{{ @BASE }}/{{ @blog->image }}" alt="undefined">
+                                <img class="img-fluid " src="{{ @BASE }}/{{ @blog->image }}" alt="undefined">
                             </div>
                             <div class="col-8">
-                                 <div class="mb-3">
+                                <div class="mb-3">
                                     <div>
                                         <h2 class="mb-0 d-inline"><a href="{{ @BASE }}/blog/{{@blog->slug}}" class="text-danger">{{ @blog->title }}</a></h2>
 
                                         <check if="{{  @SESSION.user}}">
                                             <a href="{{ @BASE }}/blog/{{@blog->slug}}/edit"> <span class="badge badge-secondary">Editar</span></a>
-           
-                                            <form id="delete-form" action="{{ @BASE }}/blog/{{ @blog->slug }}/delete" method="POST" class="d-inline" >
-                                            <button type="submit" class="badge badge-danger">Borrar</button>
+
+                                            <form id="delete-form" action="{{ @BASE }}/blog/{{ @blog->slug }}/delete" method="POST" class="d-inline">
+                                                <button type="submit" class="badge badge-danger">Borrar</button>
                                             </form>
                                         </check>
                                     </div>
 
                                     <div class="small text-muted mb-2">{{ @blog->created_at }}</div>
                                     <div class="mb-3">
-                            {{ @blog->excerpt | raw }} 
+                                        {{ @blog->excerpt | raw }}
+                                    </div>
+                                </div>
+
                             </div>
-                        </div>
-                      
-                    </div>
                 </section>
 
             </repeat>
+
+            <b>Page <b>{{ @blogs.pos + 1}}</b> of <b>{{ round(@blogs.total / @blogs.limit) }}</b>
+                <nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item   {{ @blogs.pos  == 0  ?  'disabled' : ''  }} ">
+                            <a class="page-link" href="{{ @BASE }}/blog?p={{ $_GET['p'] -1  ?? 1 }}" tabindex="-1">Previous</a>
+                        </li>
+                        <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item active">
+                            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li> -->
+                        <li class="page-item">
+                            <a class="page-link"  href="{{ @BASE }}/blog?p={{ $_GET['p'] + 1 }}">Next</a>
+                        </li>
+                    </ul>
+                </nav>
         </div>
     </div>
