f826ac5a004790961b3ccd1d66bebe0258cfb836
Luis923 <luis_gaspar923@hotmail.com>
2021-09-07 18:00:14 -0500

    Haciendo responsive el footer

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,20 +1,20 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-4">
+            <div class="col-12 col-sm-4">
                 <div class="row d-flex justify-content-center ">
                     <div class="col-12 d-flex justify-content-center">
                         <img class="imagen " src="{{@BASE }}/public/img/logo1.webp" alt="logo" loading="lazy">
                     </div>
                     <h2 class="subtitulo">CONTACTANOS:</h2>
-                    <p>atencionpsicologica@contigo-voy.com</p>
-                    <p>Cel: +51 975 511 032<br>Cel: +51 992 843 102</p>
+                    <p class="parrafoFooter mx-2">atencionpsicologica@contigo-voy.com</p>
+                    <p class="parrafoFooter">Cel: +51 975 511 032<br>Cel: +51 992 843 102</p>
                 </div>          
             </div>
-            <div class="col-4 d-flex align-items-center">
+            <div class="col-12 col-sm-4 d-flex align-items-center">
                 <div class="col-12">
                     <h2 class="subtitulo text-center">NUESTROS SERVICIOS</h2>
-                    <ul>
+                    <ul class="primeraListaServicios">
                         <li>Terapia Psicológica Online</li>
                         <li>Terapia Familiar</li>
                         <li>Terapia para la Ansiedad</li>
@@ -24,8 +24,8 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-4 d-flex align-items-center">
-                <ul>
+            <div class="col-12 col-sm-4 d-flex align-items-center">
+                <ul class="segundaListaServicios">
                     <li>Terapia para el Estrés</li>
                     <li>Terapia para niños y niñas</li>
                     <li>Terapia para Adolescentes y Jóvenes</li>
