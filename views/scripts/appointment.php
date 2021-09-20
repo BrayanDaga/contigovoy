4ccf69f26f6aef39db5cf8507aaa03bdb93ea0c1
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-09-20 14:04:33 -0500

    table-responsive

--- views/scripts/appointment.php
+++ views/scripts/appointment.php
@@ -1,23 +1,19 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.2/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/autofill/2.3.7/css/autoFill.bootstrap4.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.bootstrap4.min.css">
+
 <script src="https://cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.2/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/autofill/2.3.7/js/dataTables.autoFill.min.js"></script>
-<script src="https://cdn.datatables.net/autofill/2.3.7/js/autoFill.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.0.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.bootstrap4.min.js"></script>
-
 
 <f3:check if="{{ @SESSION.user.role  == 'doctor' ||  @SESSION.user.role  == 'admin'  }}">
     <script>
         let table = $('#myTable').DataTable({
+            responsive: true,
+            processing: true,
             ajax: {
                 url: '/citaslist',
                 dataSrc: 'data'
             },
             columns: [{
-                    data: 'id'
+                    data: 'id',
                 },
                 {
                     data: 'doctor'
